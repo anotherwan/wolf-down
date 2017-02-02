9daeb48788b3194a3b623a4091a9cf0734f440bc
anotherwan <itsanotherwan@gmail.com>
2017-02-02 17:42:46 -0500

    adding to github

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,9 @@
 const express = require('express');
 const router  = express.Router();
 
-<<<<<<< HEAD
 module.exports = (knex) => {
   router.get("/menu", (req, res) => {
+    console.log('Here i AMMM')
     knex
       .select("*")
       .from("dishes")
@@ -14,19 +14,6 @@ module.exports = (knex) => {
         res.render('menu', {results});
     });
   });
-=======
-// module.exports = (knex) => {
-//   router.get("/menu/cart", (req, res) => {
-//     knex
-//       .select("*")
-//       .from("dishes")
-//       .from("orders")
-//       .then((results) => {
-//         console.log(results)
-//         res.render('cart', {results});
-//     });
-//   });
->>>>>>> b6d3c27545ddf0977ffc4389c5d8adb8e49da7f4
 
-//   return router;
-// }
+  return router;
+}
