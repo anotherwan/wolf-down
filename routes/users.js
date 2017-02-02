3d50245e0048c254733aceaaef7f97f9561b10c8
deefourple <davedehghani@gmail.com>
2017-02-02 17:35:32 +0000

    Edited the nav bar and added info images to the home page

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ module.exports = (knex) => {
   router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("users")
+      .from("dishes")
       .then((results) => {
         res.json(results);
     });
