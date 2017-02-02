07634925f6927391f4dff73a3c758ce933927a96
anotherwan <itsanotherwan@gmail.com>
2017-02-01 21:03:53 -0500

    ran migration and seeded db

--- db/migrations/20160712210838_create_tables.js
+++ db/migrations/20160712210838_create_tables.js
@@ -8,7 +8,6 @@ exports.up = function(knex, Promise) {
     table.string('phone_number').notNullable();
     table.string('billing_info').notNullable();
   }),
-
   knex.schema.createTable('owners', function (table) {
     table.increments('id').primary().unsigned();
     table.string('full_name').notNullable();
@@ -17,35 +16,34 @@ exports.up = function(knex, Promise) {
     table.string('phone_number').notNullable();
     table.string('address').notNullable();
   }),
-
-  knex.schema.createTable('items', function (table) {
+  knex.schema.createTable('dishes', function (table) {
     table.increments('id').primary().unsigned();
+    table.string('dish_name').notNullable();
+    table.string('dish_description').notNullable();
     table.float('price').notNullable();
-    table.integer('quantity').notNullable();
     table.integer('owner_id').references('owners.id').notNullable();
-
   }),
-
- knex.schema.createTable('orders', function (table) {
+  knex.schema.createTable('orders', function (table) {
     table.increments('id').primary().unsigned();
-    table.float('totalcost').notNullable();
+    table.float('total_cost').notNullable();
     table.timestamp('time_ordered').defaultTo(knex.fn.now()).notNullable();
     table.string('pickup_eta');
-    table.integer('item_id').references('items.id').notNullable();
     table.integer('owner_id').references('owners.id').notNullable();
     table.integer('customer_id').references('customers.id').notNullable();
+  }),
+ knex.schema.createTable('order_dishes', function (table) {
+    table.integer('quantity').notNullable();
+    table.integer('dishes_id').references('dishes.id').notNullable();
+    table.integer('order_id').references('orders .id').notNullable();
   })
-
-
   ]);
-
 };
-
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.dropTable('customers'),
     knex.schema.dropTable('owners'),
-    knex.schema.dropTable('items'),
-    knex.schema.dropTable('orders')
+    knex.schema.dropTable('dishes'),
+    knex.schema.dropTable('orders'),
+    knex.schema.dropTAble('order_dishes')
   ]);
 };
